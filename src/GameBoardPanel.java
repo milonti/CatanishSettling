6bc8c26a882ea4296c4cfd5809a31abbf4428118
naeleejang <nae.lee.jang@gmail.com>
2013-04-27 16:05:10 -0300

    Update GameBoardPanel.java

--- src/GameBoardPanel.java
+++ src/GameBoardPanel.java
@@ -11,6 +11,8 @@ public class GameBoardPanel extends JPanel {
 	public GameBoardPanel(GameLogic gl) {
 		this.gl = gl;
 		this.hexRolls = gl.hexRolls;
+		this.resDistr = gl.resDistr;
+		this.portOrder = gl.portOrder;
 	}
 	
 	//Keep these in a 30 60 90 ratio
@@ -25,6 +27,8 @@ public class GameBoardPanel extends JPanel {
 	private GameLogic gl;
 	
 	private ArrayList<Integer> hexRolls;
+	private ArrayList<Resource> resDistr;
+	private ArrayList<Resource> portOrder;
 	
 	
 	public void paint(Graphics g){
@@ -43,31 +47,45 @@ public class GameBoardPanel extends JPanel {
 		
 		
 		//Set up board
-		g.setColor(Color.magenta);
-		g.fillPolygon(makeHex(startX - 4*b,startY));
-		g.setColor(Color.white);
-		g.fillPolygon(makeHex(startX + b,startY + a + c));
-		g.fillPolygon(makeHex(startX - b, startY + a + c));
-		g.fillPolygon(makeHex(startX + b*3, startY - (a + c) ));
-		g.fillPolygon(makeHex(startX, startY - 2*(a + c) ));
-		g.setColor(Color.green);
-		g.fillPolygon(makeHex(startX + 2*b,startY ));
-		g.fillPolygon(makeHex(startX + 2*b,startY + 2*(a + c)));
-		g.fillPolygon(makeHex(startX - 2*b,startY ));
+		// ordered from left to right, top to bottom.
+		g.setColor(resDistr.get(0).getColor());
 		g.fillPolygon(makeHex(startX - 2*b,startY - 2*(a + c)));
-		g.setColor(Color.yellow);
-		g.fillPolygon(makeHex(startX + 0*b,startY + 0*(a + c)));
+		g.setColor(resDistr.get(1).getColor());
+		g.fillPolygon(makeHex(startX, startY - 2*(a + c) ));
+		g.setColor(resDistr.get(2).getColor());
 		g.fillPolygon(makeHex(startX + 2*b,startY - 2*(a + c)));
-		g.fillPolygon(makeHex(startX + 4*b,startY + 0*(a + c)));
-		g.fillPolygon(makeHex(startX + 0*b,startY + 2*(a + c)));
-		g.setColor(Color.red);
-		g.fillPolygon(makeHex(startX + 1*b,startY - 1*(a + c)));
+		g.setColor(resDistr.get(3).getColor());
 		g.fillPolygon(makeHex(startX - 3*b,startY - 1*(a + c)));
-		g.fillPolygon(makeHex(startX - 3*b,startY + 1*(a + c)));
-		g.setColor(Color.gray);
+		g.setColor(resDistr.get(4).getColor());
 		g.fillPolygon(makeHex(startX - 1*b,startY - 1*(a + c)));
+		g.setColor(resDistr.get(5).getColor());
+		g.fillPolygon(makeHex(startX + 1*b,startY - 1*(a + c)));
+		g.setColor(resDistr.get(6).getColor());
+		g.fillPolygon(makeHex(startX + b*3, startY - (a + c) ));
+		g.setColor(resDistr.get(7).getColor());
+		g.fillPolygon(makeHex(startX - 4*b,startY));
+		g.setColor(resDistr.get(8).getColor());
+		g.fillPolygon(makeHex(startX - 2*b,startY ));
+		g.setColor(resDistr.get(9).getColor());
+		g.fillPolygon(makeHex(startX + 0*b,startY + 0*(a + c)));
+		g.setColor(resDistr.get(10).getColor());
+		g.fillPolygon(makeHex(startX + 2*b,startY ));
+		g.setColor(resDistr.get(11).getColor());
+		g.fillPolygon(makeHex(startX + 4*b,startY + 0*(a + c)));
+		g.setColor(resDistr.get(12).getColor());
+		g.fillPolygon(makeHex(startX - 3*b,startY + 1*(a + c)));
+		g.setColor(resDistr.get(13).getColor());
+		g.fillPolygon(makeHex(startX - b, startY + a + c));
+		g.setColor(resDistr.get(14).getColor());
+		g.fillPolygon(makeHex(startX + b,startY + a + c));
+		g.setColor(resDistr.get(15).getColor());
 		g.fillPolygon(makeHex(startX + 3*b,startY + 1*(a + c)));
+		g.setColor(resDistr.get(16).getColor());
 		g.fillPolygon(makeHex(startX - 2*b,startY + 2*(a + c)));
+		g.setColor(resDistr.get(17).getColor());
+		g.fillPolygon(makeHex(startX + 0*b,startY + 2*(a + c)));
+		g.setColor(resDistr.get(18).getColor());
+		g.fillPolygon(makeHex(startX + 2*b,startY + 2*(a + c)));
 		
 		//Draw border lines
 		g.setColor(Color.black);
@@ -113,15 +131,25 @@ public class GameBoardPanel extends JPanel {
 		g.fillOval(214 + 4*b, 272 + 2*(a + c), c, c);
 		g.fillOval(214 + 6*b, 272 + 2*(a + c), c, c);
 		//Docks
-		g.fillOval(214 + 5*b, 272 + 3*(a + c), c, c);
-		g.fillOval(214 + 8*b, 272 + 2*(a + c), c, c);
-		g.fillOval(214 + 1*b, 272 + 3*(a + c), c, c);
-		g.fillOval(214 - 1*b, 272 + (a + c), c, c);
-		g.fillOval(214 - 1*b, 272 - (a + c), c, c);
-		g.fillOval(214 + 5*b, 272 - 3*(a + c), c, c);
-		g.fillOval(214 + 8*b, 272 - 2*(a + c), c, c);
-		g.fillOval(214 + 1*b, 272 - 3*(a + c), c, c);
-		g.fillOval(214 + 10*b, 272 - 0*(a + c), c, c);
+		// ordered clockwise starting from the top left corner next to Resource #1
+		g.setColor(portOrder.get(0).getColor());
+		g.fillOval(214 + 10*b, 272 - 0*(a + c), c, c); //? top left
+		g.setColor(portOrder.get(1).getColor());
+		g.fillOval(214 + 5*b, 272 - 3*(a + c), c, c); //S
+		g.setColor(portOrder.get(2).getColor());
+		g.fillOval(214 + 8*b, 272 - 2*(a + c), c, c); //? top right edge middle
+		g.setColor(portOrder.get(3).getColor());
+		g.fillOval(214 + 1*b, 272 - 3*(a + c), c, c); //? middle right corner
+		g.setColor(portOrder.get(4).getColor());
+		g.fillOval(214 + 8*b, 272 + 2*(a + c), c, c); //B
+		g.setColor(portOrder.get(5).getColor());
+		g.fillOval(214 + 5*b, 272 + 3*(a + c), c, c); //L
+		g.setColor(portOrder.get(6).getColor());
+		g.fillOval(214 + 1*b, 272 + 3*(a + c), c, c); //? bottom left
+		g.setColor(portOrder.get(7).getColor());
+		g.fillOval(214 - 1*b, 272 + (a + c), c, c); //W
+		g.setColor(portOrder.get(8).getColor());
+		g.fillOval(214 - 1*b, 272 - (a + c), c, c); //O
 		
 		g.setColor(Color.black);
 		g.drawOval(214 + 2*b, 272, c, c);
@@ -169,19 +197,19 @@ public class GameBoardPanel extends JPanel {
 		g.drawString(hexRolls.get(13).toString(), 301 + 1*b, 168 + 3*(a+c));
 		g.drawString(hexRolls.get(14).toString(), 301 + 3*b, 168 + 3*(a+c));
 		g.drawString(hexRolls.get(15).toString(), 301 + 5*b, 168 + 3*(a+c));
-		g.drawString(hexRolls.get(13).toString(), 301 + 0*b, 168 + 4*(a+c));
-		g.drawString(hexRolls.get(14).toString(), 301 + 2*b, 168 + 4*(a+c));
-		g.drawString(hexRolls.get(15).toString(), 301 + 4*b, 168 + 4*(a+c));
-		//Once again, DOCKS
-		g.drawString(" ?", 301 - 1*b, 168 - 1*(a+c));
-		g.drawString(" S", 301 + 3*b, 168 - 1*(a+c));
-		g.drawString(" ?", 301 + 6*b, 168 + 0*(a+c));
-		g.drawString(" ?", 301 + 8*b, 168 + 2*(a+c));
-		g.drawString(" B", 301 + 6*b, 168 + 4*(a+c)); 
-		g.drawString(" ?", 301 - 1*b, 168 + 5*(a+c));
-		g.drawString(" L", 301 + 3*b, 168 + 5*(a+c));
-		g.drawString(" I", 301 - 3*b, 168 + 1*(a+c));
-		g.drawString(" W", 301 - 3*b, 168 + 3*(a+c));
+		g.drawString(hexRolls.get(16).toString(), 301 + 0*b, 168 + 4*(a+c));
+		g.drawString(hexRolls.get(17).toString(), 301 + 2*b, 168 + 4*(a+c));
+		g.drawString(hexRolls.get(18).toString(), 301 + 4*b, 168 + 4*(a+c));
+		// ordered clockwise starting from the top left corner next to Resource #1
+		g.drawString(" "+portOrder.get(0).toPortString(), 301 - 1*b, 168 - 1*(a+c));
+		g.drawString(" "+portOrder.get(1).toPortString(), 301 + 3*b, 168 - 1*(a+c));
+		g.drawString(" "+portOrder.get(2).toPortString(), 301 + 6*b, 168 + 0*(a+c));
+		g.drawString(" "+portOrder.get(3).toPortString(), 301 + 8*b, 168 + 2*(a+c));
+		g.drawString(" "+portOrder.get(4).toPortString(), 301 + 6*b, 168 + 4*(a+c)); 
+		g.drawString(" "+portOrder.get(5).toPortString(), 301 + 3*b, 168 + 5*(a+c));
+		g.drawString(" "+portOrder.get(6).toPortString(), 301 - 1*b, 168 + 5*(a+c));
+		g.drawString(" "+portOrder.get(7).toPortString(), 301 - 3*b, 168 + 3*(a+c));
+		g.drawString(" "+portOrder.get(8).toPortString(), 301 - 3*b, 168 + 1*(a+c));
 	}
 	
 	public Polygon makeHex(int x, int y){
