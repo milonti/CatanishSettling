439b3d856c4de9bc024eecbc086c44f9b3a61406
naeleejang <nae.lee.jang@gmail.com>
2013-04-27 17:16:58 -0300

    Create Edge.java

---
+++ src/Edge.java
@@ -0,0 +1,11 @@
+
+public class Edge {
+  public int vertIndex1;
+	public int vertIndex2;
+	
+	public Edge(int vertIndex1,int vertIndex2)
+	{
+		this.vertIndex1=vertIndex1;
+		this.vertIndex2=vertIndex2;
+	}
+}
